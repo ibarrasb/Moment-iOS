4d7a327e7c5d9c1a5d7665fd4e97320b228588f5
Eddie Ibarra <eddieibarra43@gmail.com>
2023-08-24 16:34:37 -0500

    ios

---
+++ Moment-iOS/Moment-iOS/Moment_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Moment_iOSApp.swift
+//  Moment-iOS
+//
+//  Created by eduardo.ibarra on 8/24/23.
+//
+
+import SwiftUI
+
+@main
+struct Moment_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
